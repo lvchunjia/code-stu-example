0e892075b334b45684efb271173aa29d05c57f96
jiumingmao <879021430@qq.com>
2023-09-24 20:49:33 +0800

    style: 🎨 格式化 datav libs

--- imooc-datav/imooc-datav-libs-dev/src/components/ImoocFlyBox/ImoocFlyBox.vue
+++ imooc-datav/imooc-datav-libs-dev/src/components/ImoocFlyBox/ImoocFlyBox.vue
@@ -1,30 +1,103 @@
 <template>
-  <div class="imooc-fly-box" :ref="refName">
-    <svg :width="width" :height="height">
+  <div
+    class="imooc-fly-box"
+    :ref="refName"
+  >
+    <svg
+      :width="width"
+      :height="height"
+    >
       <defs>
-        <path :id="pathId" :d="path" fill="none"></path>
+        <!-- 边框 -->
+        <path
+          :id="pathId"
+          :d="path"
+          fill="none"
+        ></path>
+
+        <!-- 蒙版，可以将重合的区域颜色进行融合 -->
         <mask :id="maskId">
-          <circle :r="starLength" cx="0" cy="0" :fill="`url(#${radialGradientId})`">
-            <animateMotion :dur="`${duration}s`" :path="path" rotate="auto" repeatCount="indefinite" />
+          <circle
+            :r="starLength"
+            cx="0"
+            cy="0"
+            :fill="`url(#${radialGradientId})`"
+          >
+            <animateMotion
+              :dur="`${duration}s`"
+              :path="path"
+              rotate="auto"
+              repeatCount="indefinite"
+            />
           </circle>
         </mask>
-        <!-- <linearGradient :id="radialGradientId">
-          <stop offset="0%" stop-color="red" stop-opacity="1"></stop>
-          <stop offset="50%" stop-color="green"></stop>
-          <stop offset="100%" stop-color="blue" stop-opacity="0.5"></stop>
-        </linearGradient> -->
+
         <!-- linearGradient是一种线性渐变色的实现方案 -->
-        <radialGradient :id="radialGradientId" cx="50%" cy="50%" fx="100%" fy="50%" r="50%">
-          <stop offset="0%" stop-color="#fff" stop-opacity="1"></stop>
-          <stop offset="100%" stop-color="#fff" stop-opacity="0"></stop>
-        </radialGradient>
+        <!-- <linearGradient :id="radialGradientId">
+          <stop
+            offset="0%"
+            stop-color="red"
+            stop-opacity="1"
+          ></stop>
+          <stop
+            offset="50%"
+            stop-color="green"
+          ></stop>
+          <stop
+            offset="100%"
+            stop-color="blue"
+            stop-opacity="0.5"
+          ></stop>
+        </linearGradient> -->
+
         <!-- fx和fy的作用时设置内圈的颜色起始放射位置，cx和cy表示的是外圈颜色放射范围，r表示放射半径 -->
         <!-- radialGradient是一种放射性渐变颜色的实现方案 -->
+        <radialGradient
+          :id="radialGradientId"
+          cx="50%"
+          cy="50%"
+          fx="100%"
+          fy="50%"
+          r="50%"
+        >
+          <stop
+            offset="0%"
+            stop-color="#fff"
+            stop-opacity="1"
+          ></stop>
+          <stop
+            offset="100%"
+            stop-color="#fff"
+            stop-opacity="0"
+          ></stop>
+        </radialGradient>
       </defs>
-      <!-- <rect x="100" y="100" width="100" height="100" fill="url(#linear-gradient)" />
-      <circle cx="200" cy="200" r="100" fill="url(#radial-gradient)" /> -->
-      <use :href="`#${pathId}`" stroke-width="1" :stroke="lineColor"></use>
-      <use :href="`#${pathId}`" stroke-width="3" :stroke="starColor" :mask="`url(#${maskId})`"></use>
+
+      <!-- <rect
+        x="100"
+        y="100"
+        width="100"
+        height="100"
+        fill="url(#linear-gradient)"
+      />
+      <circle
+        cx="200"
+        cy="200"
+        r="100"
+        fill="url(#radial-gradient)"
+      /> -->
+
+      <use
+        :href="`#${pathId}`"
+        stroke-width="1"
+        :stroke="lineColor"
+      ></use>
+      <use
+        :href="`#${pathId}`"
+        stroke-width="3"
+        :stroke="starColor"
+        :mask="`url(#${maskId})`"
+      ></use>
     </svg>
     <div class="imooc-fly-box-content">
       <slot></slot>
@@ -33,49 +106,57 @@
 </template>
 
 <script>
-import {v4 as uuidv4} from 'uuid'
-import { ref, onMounted, getCurrentInstance, computed } from 'vue';
+import { v4 as uuidv4 } from "uuid";
+import { ref, onMounted, getCurrentInstance, computed } from "vue";
 // getCurrentInstance的作用是拿到vue的实例
+
 export default {
-  name:'ImoocFlyBox',
-  props:{
-    lineColor:{
-      type:String,
-      default:'#235fa7'
+  name: "ImoocFlyBox",
+  props: {
+    lineColor: {
+      type: String,
+      default: "#235fa7",
     },
-    starColor:{
-      type:String,
-      default:'#4fd2dd'
+    starColor: {
+      type: String,
+      default: "#4fd2dd",
     },
-    starLength:{
-      type:[String,Number],
-      default:50
+    starLength: {
+      type: [String, Number],
+      default: 50,
     },
-    duration:{
-      type:[Number, String],
-      default:3
-    }
+    duration: {
+      type: [Number, String],
+      default: 3,
+    },
   },
   setup(ctx) {
-    const uuid = uuidv4()
-    const width = ref(0)
-    const height = ref(0)
-    const refName = ''
-    const pathId = `${refName}-path-${uuid}`
-    const radialGradientId = `${refName}-gradient-${uuid}`
-    const maskId = `${refName}-mask-${uuid}`
-    const path = computed(() => 
-      `M5 5 L${width.value - 5} 5 L${width.value - 5} ${height.value - 5} L5 ${height.value - 5} Z`
-    )
+    const uuid = uuidv4();
+    const width = ref(0);
+    const height = ref(0);
+    const refName = "";
+    const pathId = `${refName}-path-${uuid}`;
+    const radialGradientId = `${refName}-gradient-${uuid}`;
+    const maskId = `${refName}-mask-${uuid}`;
+
+    const path = computed(
+      () =>
+        `M5 5 L${width.value - 5} 5 L${width.value - 5} ${
+          height.value - 5
+        } L5 ${height.value - 5} Z`
+    );
+
     const init = () => {
-      const instance = getCurrentInstance()
-      const dom = instance.ctx.$refs[refName]
-      width.value = dom.clientWidth
-      height.value = dom.clientHeight
-    }
+      const instance = getCurrentInstance();
+      const dom = instance.ctx.$refs[refName];
+      width.value = dom.clientWidth;
+      height.value = dom.clientHeight;
+    };
+
     onMounted(() => {
-      init()
-    })
+      init();
+    });
+
     return {
       pathId,
       radialGradientId,
@@ -83,10 +164,10 @@ export default {
       path,
       refName,
       width,
-      height
-    }
-  }
-}
+      height,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
--- imooc-datav/imooc-datav-libs-dev/src/components/ImoocLoading/ImoocLoading.vue
+++ imooc-datav/imooc-datav-libs-dev/src/components/ImoocLoading/ImoocLoading.vue
@@ -1,16 +1,65 @@
 <template>
   <div class="imooc-loading">
-    <svg :width="width" :height="height" viewBox="0 0 50 50" preserveAspectRatio="xMidYMid meet">
-      <circle cx="25" cy="25" r=22 fill="none" stroke-width="3" :stroke="outsideColor" stroke-dasharray="34" stroke-linecap="round">
-        <animateTransform attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" :dur="`${+duration}s`" repeatCount="indefinite" />
-        <animate attributeName="stroke" :values="outsideColorAnimation" :dur="`${+duration * 2}s`" repeatCount="indefinite" />
-      </circle>
-      <!-- from="0 25 25" to="360 25 25" 这里的25 25是改变圆心 -->
+    <svg
+      :width="width"
+      :height="height"
+      viewBox="0 0 50 50"
+      preserveAspectRatio="xMidYMid meet"
+    >
+      <!-- stroke-dasharray= 2 * 3.14 / 4 = 34 -->
       <!-- stroke-linecap="round"这个属性的作用是将线条的边缘变成圆角 -->
-      <circle cx="25" cy="25" r="12" fill="none" stroke-width="3" :stroke="insideColor" stroke-dasharray="19" stroke-linecap="round">
-        <animateTransform attributeName="transform" type="rotate" values="360 25 25;0 25 25" :dur="`${+duration}s`" repeatCount="indefinite" />
-        <animate attributeName="stroke" :values="insideColorAnimation" :dur="`${+duration * 2}s`" repeatCount="indefinite" />
-        <!-- from 和to的值也可以用values来替换 -->
+      <circle
+        cx="25"
+        cy="25"
+        r="22"
+        fill="none"
+        stroke-width="3"
+        :stroke="outsideColor"
+        stroke-dasharray="34"
+        stroke-linecap="round"
+      >
+        <!-- from="0 25 25" to="360 25 25" 这里的25 25是改变圆心 -->
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          from="0 25 25"
+          to="360 25 25"
+          :dur="`${+duration}s`"
+          repeatCount="indefinite"
+        />
+        <animate
+          attributeName="stroke"
+          :values="outsideColorAnimation"
+          :dur="`${+duration * 2}s`"
+          repeatCount="indefinite"
+        />
+      </circle>
+
+      <circle
+        cx="25"
+        cy="25"
+        r="12"
+        fill="none"
+        stroke-width="3"
+        :stroke="insideColor"
+        stroke-dasharray="19"
+        stroke-linecap="round"
+      >
+        <!-- from 和 to 的值也可以用values来替换 -->
+        <!-- values = from;to -->
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          values="360 25 25;0 25 25"
+          :dur="`${+duration}s`"
+          repeatCount="indefinite"
+        />
+        <animate
+          attributeName="stroke"
+          :values="insideColorAnimation"
+          :dur="`${+duration * 2}s`"
+          repeatCount="indefinite"
+        />
       </circle>
     </svg>
     <div class="imooc-loading-content">
@@ -20,44 +69,44 @@
 </template>
 
 <script>
-import {computed} from 'vue'
+import { computed } from "vue";
 export default {
-  name:'ImoocLoading',
-  props:{
-    width:{
-      type:[Number,String],
-      default:50
+  name: "ImoocLoading",
+  props: {
+    width: {
+      type: [Number, String],
+      default: 50,
     },
-    height:{
-      type:[Number,String],
-      default:50
+    height: {
+      type: [Number, String],
+      default: 50,
     },
-    outsideColor:{
-      type:String,
-      default:'#3be6cb'
+    outsideColor: {
+      type: String,
+      default: "#3be6cb",
     },
-     insideColor:{
-      type:String,
-      default:'#02bcfe'
+    insideColor: {
+      type: String,
+      default: "#02bcfe",
     },
-    duration:{
-      type:[Number, String],
-      default:2
-    }
+    duration: {
+      type: [Number, String],
+      default: 2,
+    },
   },
   setup(ctx) {
-    const outsideColorAnimation = computed(() => 
-      `${ctx.outsideColor};${ctx.insideColor};${ctx.outsideColor}`
-    )
-    const insideColorAnimation = computed(() => 
-      `${ctx.insideColor};${ctx.outsideColor};${ctx.insideColor}`
-    )
-    return{
+    const outsideColorAnimation = computed(
+      () => `${ctx.outsideColor};${ctx.insideColor};${ctx.outsideColor}`
+    );
+    const insideColorAnimation = computed(
+      () => `${ctx.insideColor};${ctx.outsideColor};${ctx.insideColor}`
+    );
+    return {
       outsideColorAnimation,
-      insideColorAnimation
-    }
-  }
-}
+      insideColorAnimation,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
